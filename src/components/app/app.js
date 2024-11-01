aaf70a61122e216f7c1014747722f8c6b3872433
Artem Zolin <artem.zolin1@yandex.ru>
2024-11-01 14:15:32 +0300

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import './app.css'
+
+export default class App extends Component {
+
+  render (){
+    console.log("lol")
+
+    return(
+      <div className="app">
+        <div>Hello world</div>
+      </div>
+    )
+  }
+}
